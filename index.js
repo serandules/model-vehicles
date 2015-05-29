dacbd87281858e088d0e3412b89058886d34de27
ruchiraw <ruchira.wageesha@gmail.com>
2015-05-29 23:25:25 +0530

    updating

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var model;
 try {
     model = mongoose.model('Vehicle') || require('/model');
 } catch (e) {
-    model = require('/model');
+    model = require('./model');
 }
 
 module.exports = model;
